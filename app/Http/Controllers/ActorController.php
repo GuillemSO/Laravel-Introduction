46dfab2a4d4c0f5cb27fd60e5f0c75a51e88d839
GuillemS <guim.san.oli@gmail.com>
2025-04-13 12:59:07 +0200

    #25 implementation of ORM Eloquent in ActorController

--- app/Http/Controllers/ActorController.php
+++ app/Http/Controllers/ActorController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Actor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 
 class ActorController extends Controller
 {
+
+
     public function listActors()
     {
-        $actorsData = DB::table('actors')->get()->map(function ($actor) {
-            return (array) $actor; // Convertir cada objeto stdClass a un array para que en la vista se muestre con el array_keys sin problemas
-        })->toArray();
-    
         $title = "Listado de todos los actores";
-    
+        $actorsData = Actor::all();
         return view("actors.list", ["actors" => $actorsData, "title" => $title]);
     }
 
@@ -30,15 +29,11 @@ class ActorController extends Controller
         $startYear = (int) $decade;
         $endYear = $startYear + 9;
 
-        $actorsData = DB::table('actors')
-            ->whereBetween(DB::raw("YEAR(STR_TO_DATE(birthdate, '%Y-%m-%d'))"), [$startYear, $endYear])
-            ->get()
-            ->map(fn($actor) => (array) $actor)
-            ->toArray();
-
         $title = "Actores nacidos entre $startYear y $endYear";
+        
+        $actors = Actor::whereBetween(DB::raw('YEAR(birthdate)'), [$startYear, $endYear])->get();
 
-        return view("actors.list", ["actors" => $actorsData, "title" => $title]);
+        return view('actors.list', ['actors' => $actors, 'title' => $title]);
     }
 
     
@@ -46,28 +41,16 @@ class ActorController extends Controller
     public function countActors()
     {
         $title = "Número de Actores";
-        $actorsData = DB::table('actors')->count();
+         $countActors = Actor::count();
 
-        return view("actors.count", ["numberActors" => $actorsData, "title" => $title]);
+        return view("actors.count", ["numberActors" => $countActors, "title" => $title]);
         
     }
 
     public function destroy($id)
     {
-        $actor = DB::table('actors')->where('id', $id)->first();
+        $status = Actor::where('id', $id)->delete();
 
-        if (!$actor) {
-            return response()->json([
-                'action' => 'delete',
-                'status' => false
-            ], 404);
-        }
-
-        $deleted = DB::table('actors')->where('id', $id)->delete();
-
-        return response()->json([
-            'action' => 'delete',
-            'status' => $deleted ? true : false
-        ]);
+       return response()->json(["action" => "delete", "status" => $status]);
     }
 }
